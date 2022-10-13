fe580f4dd52e1a0b1fc0351600e48e7e9f370bba
alexdennis2001 <alejandrodennis2001@outlook.com>
2022-10-12 19:45:18 -0500

    Archivos no usados

--- assets/js/scripts/audio_player_basic.js
+++
@@ -1,7 +0,0 @@
-function play(elm) {
-  let etiquetaAudio = document.createElement("audio");
-  let $aud = $(elm).children("audio").first();
-  let urlAudio=$aud.attr("src");
-  etiquetaAudio.setAttribute("src", urlAudio);
-  etiquetaAudio.play();
-}
